1eb5eca9139d96a22c7512f976aad308de116c75
bmana21 <bmana21@freeuni.edu.ge>
2023-11-10 03:58:10 +0400

    Updated Resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am Besarioni Managadze, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'https://i.postimg.cc/cJsSkWjT/received-415047537442931.jpg',
     imagebw: 'https://i.postimg.cc/cJsSkWjT/received-415047537442931.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1loZ6mKjINRly87vB4e1qMyTMiNv8w_CW/view?usp=drivesdk'
+    resumePdf: 'https://drive.google.com/file/d/1feZChq93eM0DvJ_jpSTqWB8DzPyKBjda/view?usp=sharing'
 }
